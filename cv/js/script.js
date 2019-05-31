eb1f7e25a0a952a772d4ecd7f25abd0f19360415
Menders01 <43556459+Menders01@users.noreply.github.com>
2019-05-31 17:42:13 +0200

    Add files via upload

---
+++ cv/js/script.js
@@ -0,0 +1,12 @@
+/*$(function() {
+	$('.navbar a, footer a').on('click', function(e) {
+        e.preventDefault();
+        var scrollTo = $(this).attr('href'); // retrieve the hash using .attr()
+
+        if (scrollTo != null && scrollTo != '') {
+            $('html, body').animate({
+                scrollTop: $(scrollTo).offset().top
+            }, 500);
+        }
+    });
+})*/
